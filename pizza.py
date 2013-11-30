a387210245554b88ae2f4cba358b72f22e2505ac
mirobeka <mirobeka@gmail.com>
2013-11-30 13:02:55 +0100

    refactor controller - new views - new naming
    modified:   pizza.py

--- pizza.py
+++ pizza.py
@@ -9,12 +9,16 @@ app.debug = True
 
 @app.route("/")
 def index():
-  return redirect(url_for("pizzaday"))
+  return redirect(url_for("welcome"))
 
-@app.route("/pizzaday/")
-def pizzaday():
+@app.route("/welcome/")
+def welcome():
+  return render_template("welcome.jinja")
+
+@app.route("/pizzaorder/")
+def pizzaorder():
   pizzas = chommi.gimme_dat_pizzas()
-  return render_template("pizzaday.jinja", pizzas=pizzas)
+  return render_template("select_pizza.jinja", pizzas=pizzas)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
