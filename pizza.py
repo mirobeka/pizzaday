84ffdfbd9605ed56e60f0e64080b092e2d032014
mirobeka <mirobeka@gmail.com>
2013-12-01 02:04:15 +0100

    Fixes restaurant import + add start session functionality
    modified:   pizza.py

--- pizza.py
+++ pizza.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
-import chommi
+from flask import request
 import sessions
 
 app = Flask(__name__)
@@ -19,20 +20,32 @@ def welcome():
 @app.route("/<session_name>/orderpizza/")
 def pizzaorder(session_name):
   session = sessions.get_session(session_name)
-  restaurant = __import__(session.get("info", "restaurant"))
+  restaurant_name = session.get("info", "restaurant")
+  restaurant = __import__("pizza_places."+restaurant_name, fromlist=[restaurant_name])
   pizzas = restaurant.gimme_dat_pizzas()
   return render_template("select_pizza.jinja", pizzas=pizzas)
 
-@app.route("/startsession/")
+@app.route("/startsession/", methods=["GET", "POST"])
 def start_session():
-  return render_template("start_session.jinja")
+  if request.method == "POST":
+    return start_new_session(request)
+  else:
+    return render_template("start_session.jinja")
 
 @app.route("/activesessions/")
-def start_pizza_session():
+def active_sessions():
   session_list = sessions.get_session_list()
   if len(session_list) <= 0:
     return render_template("no_active_sessions.jinja")
   return render_template("active_sessions.jinja", sessions=session_list)
+
+
+##
+# Functions
+
+def start_new_session(request):
+  sessions.create_session(request.form["session_name"], request.form)
+  return redirect_to(url_for("active_sessions"))
 
 if __name__ == "__main__":
   app.run()
