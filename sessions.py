81c3de8d0e8d5f0c78c3095bc3edb0bed45f5238
Miroslav Beka <mirobeka@gmail.com>
2014-09-17 23:47:41 +0200

    remove that file system shit, clean for using db
    modified:   sessions.py

--- sessions.py
+++ sessions.py
@@ -1,69 +1,26 @@
 from __future__ import print_function
-import ConfigParser
-import os
-
-def get_session(session_name):
-  session = load_session(session_name)
-  return session
 
 def get_session_list():
-  if exists_or_create_session_folder():
-    sessions = {}
-    for root, dirnames, files in os.walk("sessions"):
-      for session_name in files:
-        sessions[session_name] = load_session(session_name)
-    return sessions
-  return {}
+    pass
+
+def pizza_places():
+    """Returns list of available pizza places
+
+    :returns: list of touples (place_name, place_id)
+    """
+    pass
 
 def create_session(session_name, data):
-  session = ConfigParser.ConfigParser()
-  session.add_section("info")
-  session.add_section("orders")
-  session.add_section("extra")
-  session.add_section("size")
+    pass
 
-  session.set("info", "deadline", data["deadline"])
-  session.set("info", "restaurant", data["restaurant"])
-  session.set("info", "approx_lunch", data["approx_lunch"])
-  session.set("info", "recipients", data["recipients"])
-
-  save_session(session_name, session)
-
-def add_order_to_session(session_name, user_email, order):
-  session = load_session(session_name)
-  session.set("orders", user_email, order["pizza"])
-  session.set("size", user_email, order["size"])
-  if "extra" in order:
-    session.set("extra", user_email, order["extra"])
-
-  save_session(session_name, session)
+def add_order_to_session(form):
+    pass
 
 def load_session(session_name):
-  if exists_or_create_session_folder():
-    session = ConfigParser.ConfigParser()
-    file_path = os.path.join("sessions", session_name)
-    with open(file_path, "r") as fp:
-      session.readfp(fp)
-    return session
+    pass
 
 def save_session(session_name, session):
-  if exists_or_create_session_folder():
-    file_path = os.path.join("sessions", session_name)
-    if os.path.isfile(file_path):
-      with open(file_path, "w") as fp:
-        session.write(fp)
-      return True
-  return False
-
-def exists_or_create_session_folder():
-  if not os.path.isdir("sessions"):
-    try:
-      os.mkdir("sessions")
-    except:
-      print("failed to create sessions folder")
-      return False
-  return True
+    pass
 
 def sort_sessions_by_deadline(sessions):
-  # TODO: add sorting by time
-  return sessions
+    pass
