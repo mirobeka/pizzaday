4ff0b28fe656f34c631a1f262a5aefef63166ca2
Miroslav Beka <mirobeka@gmail.com>
2014-09-26 17:14:51 +0200

    Adds some very basic test
    modified:   pizza_tests.py

--- pizza_tests.py
+++ pizza_tests.py
@@ -3,15 +3,20 @@ import pizza
 import unittest
 import tempfile
 
-class FlaskrTestCase(unittest.TestCase):
+class PizzaViewTestCases(unittest.TestCase):
     def setUp(self):
         self.db_fd, pizza.app.config['DATABASE'] = tempfile.mkstemp()
-        self.app = flaskr.app.test_client()
-        pizza.init_db()
+        self.app = pizza.app.test_client()
+        pizza.check_database()
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(pizza.app.config['DATABASE'])
 
-if __name__ == '__main__':
+    def test_welcome_view(self):
+        rv = self.app.get("/", follow_redirects=True)
+        assert "Start Pizza Session" in rv.data
+        assert "Join Session" in rv.data
+
+if __name__ == "__main__":
     unittest.main()
