d56329c98448f1b77c033dfbc8be76bc247783cd
Miroslav Beka <mirobeka@gmail.com>
2015-01-13 00:35:56 +0100

    Adds form for starting session
    modified:   forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,13 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, DateTimeField, SelectField
 from wtforms.validators import Length
+from datetime import datetime
 
 class EnterEmailForm(Form):
-    email  = StringField('Email Address', validators=[Length(min=6, max=35)])
+    email = StringField('Email Address', validators=[Length(min=6, max=35)])
+
+class StartSessionForm(Form):
+    date_format = "%Y-%m-%d %H:%M"
+    deadline = DateTimeField("deadline", format=date_format)
+    recipients = StringField("recipients")
+    pizza_place = SelectField("pizza_place", choices=[("carla","Carla"),("chommi","Chommi"), ("erik","Erik")], default="carla")
