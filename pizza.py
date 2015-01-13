05cc7bb4e811bf90b8ee8950441490124b92ccf4
Miroslav Beka <mirobeka@gmail.com>
2015-01-13 13:44:06 +0100

    Some major changes and refactor in controller
    * decorators for required user email and active session
    * parsing forms is done with wtforms

--- pizza.py
+++ pizza.py
@@ -4,15 +4,16 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
-from flask import flash
 from flask import g
 from flask import session as flask_session
+from contextlib import closing
 from functools import wraps
+from datetime import datetime
 import sessions
 import sqlite3
 import os
 import logging
-from forms import EnterEmailForm
+from forms import EnterEmailForm, StartSessionForm
 
 def email_required(f):
     @wraps(f)
@@ -22,6 +23,13 @@ def email_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def session_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "active_session" not in flask_session:
+            return redirect(url_for("startsession"))
+        return f(*args, **kwargs)
+    return decorated_function
 
 logger = logging.getLogger("CONTROLLER")
 
@@ -33,7 +41,6 @@ def enter_email_post():
     emailform = EnterEmailForm()
     if emailform.validate_on_submit():
         flask_session["user_email"] = emailform.data["email"]
-        flash("Let the eating begin! {}".format(flask_session["user_email"]))
         return redirect(url_for("select_action"))
 
 @app.route("/", methods=["GET"])
@@ -48,42 +55,58 @@ def select_action():
 
 @app.route("/orderpizza/", methods=["GET"])
 @email_required
+@session_required
 def get_pizzaorder():
     # run before request
-    if "active_session" not in flask_session:
-        return redirect(url_for("get_active_sessions"))
-
     session_id = flask_session["active_session"]
-    pizzas = get_pizza_options(session_id)
+    pizzas = sessions.pizzas(session_id)
+    print(pizzas)
     return render_template("select_pizza.jinja", pizzas=pizzas)
 
 @app.route("/orderpizza/", methods=["POST"])
 @email_required
+@session_required
 def create_pizzaorder():
-    # run before request
-    if "active_session" not in flask_session:
-        return url_for("get_active_sessions")
+    if "pizza_id" in request.form:
+        pizza_id = int(request.form["pizza_id"])
+        session_id = flask_session["active_session"]
+        user_email = flask_session["user_email"]
+        sessions.add_order(session_id, user_email, pizza_id)
+        return redirect(url_for("thank_you"))
 
-    session_id = flask_session["active_session"]
-    sessions.add_order(session_id, request.form)
     return url_for("thank_you")
 
 @app.route("/thank_you/")
-def thank_you(session_name, size, pizza):
-    flask_session.pop("active_session")
-    return render_template("thank_you.jinja")
+@email_required
+@session_required
+def thank_you():
+    session_id = flask_session.pop("active_session")
+    user_email = flask_session.pop("user_email")
+
+    order = sessions.get_order(session_id, user_email)
+
+    return render_template("thank_you.jinja", order=order)
 
 @app.route("/startsession/", methods=["GET"])
 @email_required
 def get_startsession():
-    pizza_places = sessions.pizza_places()
-    return render_template("start_session.jinja", pizza_places=pizza_places)
+    form = StartSessionForm()
+    return render_template("start_session.jinja", form=form)
 
 @app.route("/startsession/", methods=["POST"])
 def create_session():
-    session_id= sessions.create_session(request.form)
-    flask_session["active_session"] = session_id
-    return url_for("get_pizzaorder")
+    form = StartSessionForm()
+    if form.validate_on_submit():
+        session_id = sessions.create_session(flask_session["user_email"], form.data)
+        flask_session["active_session"] = session_id
+        return redirect(url_for("get_active_sessions"))
+    return render_template("start_session.jinja", form=form)
+
+@app.route("/dashboard/", methods=["GET"])
+@email_required
+@session_required
+def session_dashboard():
+    pass
 
 @app.route("/activesessions/", methods=["GET"])
 @email_required
@@ -93,8 +116,8 @@ def get_active_sessions():
 
 @app.route("/activesessions/", methods=["POST"])
 def join_session():
-    flask_session["active_session"] = request.form["session"]
-    return url_for("get_pizzaorder")
+    flask_session["active_session"] = request.form["session_id"]
+    return redirect(url_for("get_pizzaorder"))
 
 # Database helpers
 def get_db():
@@ -104,19 +127,8 @@ def get_db():
     db.row_factory = sqlite3.Row
     return db
 
-@app.before_request
-def before_request():
-    g.db = get_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
 def init_db():
-    with app.app_context():
-        db = get_db()
+    with closing(connect_to_database()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
@@ -125,8 +137,13 @@ def check_database():
     DATABASE = app.config["DATABASE"]
     if not os.path.exists(DATABASE):
         with open(DATABASE, "w"):
-            logger.info("Creating empty database file: {}".format(DATABASE))
+            print("Creating empty database file: {}".format(DATABASE))
             init_db()
+
+def sql_query(query):
+    db = get_db()
+    db.execute(query)
+    db.commit()
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
